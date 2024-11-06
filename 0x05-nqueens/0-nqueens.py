9de1a7fde42cb29f26d6b1023980ce6464952aca
Khawla Ben hammou <benhammoukhawla99@gmail.com>
2024-11-06 18:01:33 +0100

    push

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import sys
+
+def print_solution(board):
+    solution = []
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row] == col:
+                solution.append([row, col])
+    print(solution)
+
+def is_safe(board, row, col):
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+def solve_nqueens(board, row, N):
+    if row == N:
+        print_solution(board)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1, N)
+            board[row] = -1  # backtrack
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solve_nqueens(board, 0, N)
+
+if __name__ == "__main__":
+    main()
