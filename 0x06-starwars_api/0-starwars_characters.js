ba2b83fa2a6519b75da22bf15fb26ac9f8f8e2a5
Khawla Ben hammou <kben-ham@e2r8p3.1337.ma>
2025-01-15 20:13:13 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,42 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
-  method: 'GET'
-};
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
   }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+  if (response.statusCode !== 200) {
+    console.log('Failed to fetch movie data');
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+
+  if (!movieData || !movieData.characters) {
+    console.log('No characters found for this movie');
+    return;
+  }
+
+  movieData.characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error('Error fetching character data:', error);
+        return;
       }
-    }
+
+      if (response.statusCode !== 200) {
+        console.log('Failed to fetch character data');
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
   });
-}
+});
